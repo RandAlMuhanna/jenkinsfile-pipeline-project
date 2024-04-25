68ee153b71c4d8933eac82a9e4908151b9f3c9b8
Rand AlMuhanna <110944327+RandAlMuhanna@users.noreply.github.com>
2024-04-25 03:16:04 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
     
     System.out.println("Hello again...");
 
+    System.out.println("Hello again Rand!");
+
     System.out.println("I hope this will trigger the job");
   }
 
